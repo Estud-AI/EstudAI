55e55742c364b755c2d9e9e297400a23119329c2
Gabriel Alves <gabriel.alves@dreamerstudios.io>
2025-09-29 20:14:18 -0300

    feat: created route to register user without firebase

--- backend/src/routes/user-register.ts
+++ backend/src/routes/user-register.ts
@@ -5,7 +5,7 @@ import prisma from "../lib/prisma";
 
 const router = express.Router();
 
-router.post("/user", async (req: Request, res: Response) => {
+router.post("/user-register-by-google", async (req: Request, res: Response) => {
   try {
     const parsed = UserSchema.safeParse(req.body);
     if (!parsed.success) {
@@ -30,5 +30,29 @@ router.post("/user", async (req: Request, res: Response) => {
     return res.status(500).json({ ok: false, error: "Failed to create user" });
   }
 });
+
+
+router.post("/user-register", async (req: Request, res: Response) => {
+  try {
+    const parsed = UserSchema.safeParse(req.body);
+    if (!parsed.success) {
+      return res.status(400).json({ ok: false, error: parsed.error.format() });
+    }
+
+    if (await prisma.user.findUnique({ where: { email: parsed.data.email } })) {
+      return res.status(400).json({ ok: false, error: "User with this email already exists" });
+    }
+
+    await prisma.user.create({
+      data: parsed.data,
+    });
+
+    return res.status(201).json({ ok: true });
+  } catch (err: any) {
+    console.error("Create user error:", err?.message || err);
+    return res.status(500).json({ ok: false, error: "Failed to create user" });
+  }
+});
+
 
 export default router;
